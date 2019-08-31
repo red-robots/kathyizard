00c5a8c2e35a68a694dbe33964ad536895c50aba
Lisa <annalisa.quiamco@gmail.com>
2019-08-30 22:22:32 -0600

    Added link on slider and some changes on homepage

--- template-parts/slides.php
+++ template-parts/slides.php
@@ -6,9 +6,17 @@ $slides = get_field('slides');
 	<div class="wrapper">
 		<?php if ($slides) { ?>
 		<div id="hero" class="owl-carousel">
-			<?php foreach ($slides as $sd) { ?>
+			<?php foreach ($slides as $sd) { 
+				$id = $sd['ID'];
+				$link = get_field('attachment_link',$id);
+				$before_link = '';
+				$after_link = '';
+				if($link) {
+					$before_link = '<a href="'.$link.'" target="_blank">';
+					$after_link = '</a>';
+				} ?>
 				<div class="item">
-	              <img src="<?php echo $sd['url'] ?>" alt="<?php echo $sd['title'] ?>" />
+	              <?php echo $before_link; ?><img src="<?php echo $sd['url'] ?>" alt="<?php echo $sd['title'] ?>" /><?php echo $after_link; ?>
 	            </div>
 			<?php } ?>
 		</div>
